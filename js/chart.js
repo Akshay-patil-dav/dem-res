4200221515c3266d15b360cfacdaa56429d44b75
Akshay Patil <akshaypatil22082002@gmail.com>
2024-09-10 15:48:48 +0530

    Add files via upload

--- js/chart.js
+++ js/chart.js
@@ -19,119 +19,109 @@
 
 
 // Add dragover event to allow dropping
-document.getElementById('dropBox9').addEventListener('dragover', function(e) {
+document.getElementById('dropBox15').addEventListener('dragover', function(e) {
+    e.preventDefault();  // Necessary to allow dropping
+});
+document.getElementById('dropBox10').addEventListener('dragover', function(e) {
+    e.preventDefault();  // Necessary to allow dropping
+});
+document.getElementById('dropBox4').addEventListener('dragover', function(e) {
     e.preventDefault();  // Necessary to allow dropping
 });
 document.getElementById('dropBox3').addEventListener('dragover', function(e) {
     e.preventDefault();  // Necessary to allow dropping
 });
-
-document.getElementById('dropBox2').addEventListener('dragover', function(e) {
+document.getElementById('dropBox7').addEventListener('dragover', function(e) {
     e.preventDefault();  // Necessary to allow dropping
 });
-document.getElementById('dropBox7').addEventListener('dragover', function(e) {
+document.getElementById('dropBox12').addEventListener('dragover', function(e) {
+    e.preventDefault();  // Necessary to allow dropping
+});
+document.getElementById('dropBox18').addEventListener('dragover', function(e) {
+    e.preventDefault();  // Necessary to allow dropping
+});
+document.getElementById('dropBox19').addEventListener('dragover', function(e) {
+    e.preventDefault();  // Necessary to allow dropping
+});
+document.getElementById('dropBox14').addEventListener('dragover', function(e) {
+    e.preventDefault();  // Necessary to allow dropping
+});
+document.getElementById('dropBox9').addEventListener('dragover', function(e) {
+    e.preventDefault();  // Necessary to allow dropping
+});
+document.getElementById('dropBox8').addEventListener('dragover', function(e) {
     e.preventDefault();  // Necessary to allow dropping
 });
 document.getElementById('dropBox13').addEventListener('dragover', function(e) {
     e.preventDefault();  // Necessary to allow dropping
 });
-document.getElementById('dropBox20').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
 
-document.getElementById('dropBox21').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
-document.getElementById('dropBox16').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
-
-document.getElementById('dropBox10').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
-
-
-document.getElementById('dropBox15').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
-
-
-document.getElementById('dropBox14').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
-
-document.getElementById('cardbox1').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
-
-document.getElementById('dropBox8').addEventListener('dragover', function(e) {
-    e.preventDefault();  // Necessary to allow dropping
-});
 // Add drop event to change box color**********************************************************
 
-
-document.getElementById('cardbox1').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
 
 document.getElementById('dropBox15').addEventListener('drop', function(e) {
     e.preventDefault();
     const color = e.dataTransfer.getData('color');
     e.target.style.backgroundColor = color;
 });
-document.getElementById('dropBox14').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
-document.getElementById('dropBox8').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
-document.getElementById('dropBox21').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
+
 
 document.getElementById('dropBox10').addEventListener('drop', function(e) {
     e.preventDefault();
     const color = e.dataTransfer.getData('color');
     e.target.style.backgroundColor = color;
 });
-document.getElementById('dropBox16').addEventListener('drop', function(e) {
+
+
+document.getElementById('dropBox4').addEventListener('drop', function(e) {
     e.preventDefault();
     const color = e.dataTransfer.getData('color');
     e.target.style.backgroundColor = color;
 });
-document.getElementById('dropBox20').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
-document.getElementById('dropBox13').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
-document.getElementById('dropBox7').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
-document.getElementById('dropBox2').addEventListener('drop', function(e) {
-    e.preventDefault();
-    const color = e.dataTransfer.getData('color');
-    e.target.style.backgroundColor = color;
-});
+
+
 document.getElementById('dropBox3').addEventListener('drop', function(e) {
     e.preventDefault();
     const color = e.dataTransfer.getData('color');
     e.target.style.backgroundColor = color;
 });
+
+
+document.getElementById('dropBox7').addEventListener('drop', function(e) {
+    e.preventDefault();
+    const color = e.dataTransfer.getData('color');
+    e.target.style.backgroundColor = color;
+});
+
+
+document.getElementById('dropBox12').addEventListener('drop', function(e) {
+    e.preventDefault();
+    const color = e.dataTransfer.getData('color');
+    e.target.style.backgroundColor = color;
+});
+
+
+document.getElementById('dropBox18').addEventListener('drop', function(e) {
+    e.preventDefault();
+    const color = e.dataTransfer.getData('color');
+    e.target.style.backgroundColor = color;
+});
+
+
+document.getElementById('dropBox19').addEventListener('drop', function(e) {
+    e.preventDefault();
+    const color = e.dataTransfer.getData('color');
+    e.target.style.backgroundColor = color;
+});
+
+
+document.getElementById('dropBox14').addEventListener('drop', function(e) {
+    e.preventDefault();
+    const color = e.dataTransfer.getData('color');
+    e.target.style.backgroundColor = color;
+});
+
+
 document.getElementById('dropBox9').addEventListener('drop', function(e) {
     e.preventDefault();
     const color = e.dataTransfer.getData('color');
@@ -139,5 +129,17 @@ document.getElementById('dropBox9').addEventListener('drop', function(e) {
 });
 
 
+document.getElementById('dropBox8').addEventListener('drop', function(e) {
+    e.preventDefault();
+    const color = e.dataTransfer.getData('color');
+    e.target.style.backgroundColor = color;
+});
+
+
+document.getElementById('dropBox13').addEventListener('drop', function(e) {
+    e.preventDefault();
+    const color = e.dataTransfer.getData('color');
+    e.target.style.backgroundColor = color;
+});
 
 
